28be2e8490caacef3766c67e8c266b0de9fc0977
Remy9926 <95456114+Remy9926@users.noreply.github.com>
2023-03-26 19:59:58 -0400

    Update GardenMain.java

--- garden/GardenMain.java
+++ garden/GardenMain.java
@@ -20,114 +20,114 @@ public class GardenMain {
 	        NUM_ROWS = Integer.valueOf(rfile.nextLine().split(" ")[1]);
 	        NUM_COLS = Integer.valueOf(rfile.nextLine().split(" ")[1]);
 	        
-	        if (NUM_COLS > 16) {
-	        	System.out.println("Too many plot columns.");
-	        }
-	        
-	        else {
+			if (NUM_COLS > 16) {
+				System.out.println("Too many plot columns.");
+			}
+
+			else {
 				Garden garden = new Garden(NUM_ROWS, NUM_COLS);
 				while (rfile.hasNextLine()) {
 					String line = rfile.nextLine();
-			        String[] splitString = line.split(" ");
-			        
-			        if (!splitString[0].equals("")) {
-			        	
-			        	if (splitString[0].toLowerCase().equals("plant")) {
-			        		int[] coords = getCoords(splitString[1]);
-		        			garden.plant(coords[0], coords[1], splitString[2].toLowerCase(), splitString[2].substring(0,1).toLowerCase());
-			        	}
-			        	
-			        	else if (splitString[0].toLowerCase().equals("print")) {
-			        		System.out.println("> " + line.toUpperCase());
-			        		garden.print();
-			        	}
-			        	
-			        	else if (splitString[0].toLowerCase().equals("grow")) {
-			        		int count = Integer.valueOf(splitString[1]);
-			        		
-			        		if (splitString.length == 2) {
-				        		System.out.println("> " + line.toUpperCase() + "\n");
-				        		garden.grow(count);
-			        		}
-			        		
-			        		else {
-			        			String[] coordOrPlant = splitString[2].split(",");
-			        			if (coordOrPlant.length == 2) {
-					        		System.out.println("> " + line.toUpperCase() + "\n");
-					        		int[] coords = getCoords(splitString[2]);
-			        				garden.grow(count, coords[0], coords[1]);
-			        			}
-			        			else {
-					        		System.out.println("> " + splitString[0].toUpperCase() + " " + splitString[1] + " " + splitString[2] + "\n");
-			        				garden.grow(count, coordOrPlant[0].toLowerCase());
-			        			}
-			        		}
-			        	}
-			        	
-			        	else if (splitString[0].toLowerCase().equals("harvest")) {
-			        		if (splitString.length == 1) {
-				        		System.out.println("> " + line.toUpperCase() + "\n");
-			        			garden.harvest();
-			        		}
-			        		
-			        		else {
-			        			String[] coordOrType = splitString[1].split(",");	
-			        			if (coordOrType.length == 2) {
-					        		System.out.println("> " + line.toUpperCase() + "\n");
-					        		int[] coords = getCoords(splitString[1]);
-					        		garden.harvest(coords[0], coords[1]);
-			        			}
-			        			else {
-					        		System.out.println("> " + splitString[0].toUpperCase() + " " + splitString[1] + "\n");
-			        				garden.harvest(splitString[1].toLowerCase());
-			        			}
-			        		}
-			        	}
-			        	
-			        	else if (splitString[0].toLowerCase().equals("pick")) {
-			        		if (splitString.length == 1) {
-				        		System.out.println("> " + line.toUpperCase() + "\n");
-			        			garden.pick();
-			        		}
-			        		
-			        		else {
-			        			String[] coordOrType = splitString[1].split(",");
-			        			if (coordOrType.length == 2) {
-					        		System.out.println("> " + line.toUpperCase() + "\n");
-					        		int[] coords = getCoords(splitString[1]);
-					        		garden.pick(coords[0], coords[1]);
-			        			}
-			        			else {
-					        		System.out.println("> " + splitString[0].toUpperCase() + " " + splitString[1] + "\n");
-			        				garden.pick(splitString[1].toLowerCase());
+				String[] splitString = line.split(" ");
 
-			        			}
-			        		}
-			        	}
-			        	
-			        	else if (splitString[0].toLowerCase().equals("cut")) {
-			        		if (splitString.length == 1) {
-				        		System.out.println("> " + line.toUpperCase() + "\n");
-			        			garden.cut();
-			        		}
-			        		
-			        		else {
-			        			String[] coordOrType = splitString[1].split(",");
-			        			if (coordOrType.length == 2) {
-					        		System.out.println("> " + line.toUpperCase() + "\n");
-					        		int[] coords = getCoords(splitString[1]);
-					        		garden.cut(coords[0], coords[1]);
-			        			}
-			        			else {
-					        		System.out.println("> " + splitString[0].toUpperCase() + " " + splitString[1] + "\n");
-			        				garden.cut(splitString[1].toLowerCase());
-			        			}
-			        		}
-			        	}
-			        }
-			    }
+				if (!splitString[0].equals("")) {
+
+					if (splitString[0].toLowerCase().equals("plant")) {
+						int[] coords = getCoords(splitString[1]);
+						garden.plant(coords[0], coords[1], splitString[2].toLowerCase(), splitString[2].substring(0,1).toLowerCase());
+					}
+
+					else if (splitString[0].toLowerCase().equals("print")) {
+						System.out.println("> " + line.toUpperCase());
+						garden.print();
+					}
+
+					else if (splitString[0].toLowerCase().equals("grow")) {
+						int count = Integer.valueOf(splitString[1]);
+
+						if (splitString.length == 2) {
+							System.out.println("> " + line.toUpperCase() + "\n");
+							garden.grow(count);
+						}
+
+						else {
+							String[] coordOrPlant = splitString[2].split(",");
+							if (coordOrPlant.length == 2) {
+								System.out.println("> " + line.toUpperCase() + "\n");
+								int[] coords = getCoords(splitString[2]);
+								garden.grow(count, coords[0], coords[1]);
+							}
+							else {
+								System.out.println("> " + splitString[0].toUpperCase() + " " + splitString[1] + " " + splitString[2] + "\n");
+								garden.grow(count, coordOrPlant[0].toLowerCase());
+							}
+						}
+					}
+
+					else if (splitString[0].toLowerCase().equals("harvest")) {
+						if (splitString.length == 1) {
+							System.out.println("> " + line.toUpperCase() + "\n");
+							garden.harvest();
+						}
+
+						else {
+							String[] coordOrType = splitString[1].split(",");	
+							if (coordOrType.length == 2) {
+								System.out.println("> " + line.toUpperCase() + "\n");
+								int[] coords = getCoords(splitString[1]);
+								garden.harvest(coords[0], coords[1]);
+							}
+							else {
+								System.out.println("> " + splitString[0].toUpperCase() + " " + splitString[1] + "\n");
+								garden.harvest(splitString[1].toLowerCase());
+							}
+						}
+					}
+
+					else if (splitString[0].toLowerCase().equals("pick")) {
+						if (splitString.length == 1) {
+							System.out.println("> " + line.toUpperCase() + "\n");
+							garden.pick();
+						}
+
+						else {
+							String[] coordOrType = splitString[1].split(",");
+							if (coordOrType.length == 2) {
+								System.out.println("> " + line.toUpperCase() + "\n");
+								int[] coords = getCoords(splitString[1]);
+								garden.pick(coords[0], coords[1]);
+							}
+							else {
+								System.out.println("> " + splitString[0].toUpperCase() + " " + splitString[1] + "\n");
+								garden.pick(splitString[1].toLowerCase());
+
+							}
+						}
+					}
+
+					else if (splitString[0].toLowerCase().equals("cut")) {
+						if (splitString.length == 1) {
+							System.out.println("> " + line.toUpperCase() + "\n");
+							garden.cut();
+						}
+
+						else {
+							String[] coordOrType = splitString[1].split(",");
+							if (coordOrType.length == 2) {
+								System.out.println("> " + line.toUpperCase() + "\n");
+								int[] coords = getCoords(splitString[1]);
+								garden.cut(coords[0], coords[1]);
+							}
+							else {
+								System.out.println("> " + splitString[0].toUpperCase() + " " + splitString[1] + "\n");
+								garden.cut(splitString[1].toLowerCase());
+							}
+						}
+					}
+				}
+		    	}
 	        }
-		}
+	}
 		
 		catch(java.io.FileNotFoundException e){
 			System.out.println("The specified file could not be found!");
